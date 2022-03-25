13b27997386c8fe13f4b578d09f88fecb5e9896d
Jeroen De Dauw <jeroendedauw@gmail.com>
2022-03-25 13:41:33 +0100

    Remove existing values when there are no new ones
    Fixes https://github.com/ProfessionalWiki/AutomatedValues/issues/4

---
+++ tests/Unit/TemplatedAliasesSpecTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\AutomatedValues\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use ProfessionalWiki\AutomatedValues\Domain\Template;
+use ProfessionalWiki\AutomatedValues\Domain\TemplatedAliasesSpec;
+use ProfessionalWiki\AutomatedValues\Domain\TemplateSegment;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\DataModel\Term\AliasGroup;
+use Wikibase\DataModel\Term\AliasGroupList;
+
+/**
+ * @covers \ProfessionalWiki\AutomatedValues\Domain\TemplatedAliasesSpec
+ */
+class TemplatedAliasesSpecTest extends TestCase {
+
+	public function testEmptyValuesCauseLabelRemoval(): void {
+		$spec = new TemplatedAliasesSpec(
+			[ 'en', 'de' ],
+			new Template( new TemplateSegment( '$', new PropertyId( 'P1' ), null ) )
+		);
+
+		$aliasGroupList = new AliasGroupList( [
+			new AliasGroup( 'en', [ 'foo', 'bar' ] ),
+			new AliasGroup( 'nl', [ 'baz', 'bah' ] ),
+			new AliasGroup( 'de', [ 'pew' ] ),
+		] );
+
+		$spec->applyTo( $aliasGroupList, new StatementList() );
+
+		$this->assertEquals(
+			new AliasGroupList( [
+				new AliasGroup( 'nl', [ 'baz', 'bah' ] ),
+			] ),
+			$aliasGroupList
+		);
+	}
+
+}
---
+++ tests/Unit/TemplatedLabelSpecTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\AutomatedValues\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use ProfessionalWiki\AutomatedValues\Domain\Template;
+use ProfessionalWiki\AutomatedValues\Domain\TemplatedLabelSpec;
+use ProfessionalWiki\AutomatedValues\Domain\TemplateSegment;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Statement\StatementList;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\DataModel\Term\TermList;
+
+/**
+ * @covers \ProfessionalWiki\AutomatedValues\Domain\TemplatedLabelSpec
+ */
+class TemplatedLabelSpecTest extends TestCase {
+
+	public function testEmptyValuesCauseLabelRemoval(): void {
+		$spec = new TemplatedLabelSpec(
+			[ 'en', 'de' ],
+			new Template( new TemplateSegment( '$', new PropertyId( 'P1' ), null ) )
+		);
+
+		$labels = new TermList();
+		$labels->setTextForLanguage( 'en', 'foo' );
+		$labels->setTextForLanguage( 'nl', 'bar' );
+		$labels->setTextForLanguage( 'de', 'baz' );
+
+		$spec->applyTo( $labels, new StatementList() );
+
+		$this->assertEquals(
+			new TermList( [ new Term( 'nl', 'bar' ) ] ),
+			$labels
+		);
+	}
+
+}
